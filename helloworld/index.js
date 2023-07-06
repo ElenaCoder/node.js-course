901c1828811f70568e0968189085170fa5cbefa1
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-07-06 13:14:00 +0300

    feat: add helloworld task solution

---
+++ helloworld/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const port = 3000;
+host = 'localhost';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Node.js');
+});
+
+server.listen(port, host, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
