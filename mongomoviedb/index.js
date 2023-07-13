e7d1aeed4286922763e3498a7c0e33c987c5e2d4
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-07-13 11:03:40 +0300

    feat: connect to MongoDB database

--- mongomoviedb/index.js
+++ mongomoviedb/index.js
@@ -4,6 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+//MongoDB connection
+const mongoose = require('mongoose');
+const mongoURL = 'YOUR_CONNECTION_STRING';
+mongoose.connect(mongoURL, { useNewUrlParser: true , useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
 const port = 3000;
 
 app.listen(port, () => {
