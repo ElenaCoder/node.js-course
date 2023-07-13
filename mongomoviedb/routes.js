342a1d3b83b80cd6dbd24da7e2912cb6f068f8e2
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-07-13 12:28:31 +0300

    feat: apply forced styling to routes.js file

--- mongomoviedb/routes.js
+++ mongomoviedb/routes.js
@@ -3,61 +3,65 @@ const router = express.Router();
 const Movie = require('./models/movie');
 
 // Fetch all movies
-router.get("/movies", async (req, res) => {
-  try {
-    const movies = await Movie.find();
-    res.send(movies)
-  } catch(err) {
-    return res.status(500).json({ message: err.message });
-  }
-})
+router.get('/movies', async (req, res) => {
+    try {
+        const movies = await Movie.find();
+        res.send(movies);
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+});
 
-// Fetch movie by title
-router.get("/movies/:id", async (req, res) => {
+// Fetch movie by id
+router.get('/movies/:id', async (req, res) => {
     try {
         const result = await Movie.findById({ _id: req.params.id });
         if (!result) {
-            return res.status(404).json({ message: "Movie not found" });
-          }
+            return res.status(404).json({ message: 'Movie not found' });
+        }
         res.send(result);
-    } catch(err) {
+    } catch (err) {
         return res.status(500).json({ message: err.message });
     }
-  })
+});
 
 // Add movie
-router.post("/movies", async (req, res) => {
+router.post('/movies', async (req, res) => {
     const movie = new Movie({
-      title: req.body.title,
-      director: req.body.director,
-      year: req.body.year
+        title: req.body.title,
+        director: req.body.director,
+        year: req.body.year,
     });
 
     try {
-      const newMovie = await movie.save();
-      res.status(201).json({ newMovie });
-    } catch(err) {
-      return res.status(500).json({ message: err.message });
+        const newMovie = await movie.save();
+        res.status(201).json({ newMovie });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
     }
-  })
+});
 
 // Delete movie by title
-router.delete("/movies", async (req, res) => {
+router.delete('/movies', async (req, res) => {
     try {
         const result = await Movie.deleteMany({ title: req.body.title });
         res.status(200).json(result);
-      } catch (err) {
-        return res.status(500).json({ message: err.message });
-      }
-})
-
-// Update movie by id
-router.put("/movies/:id", async (req, res) => {
-    try {
-        const result = await Movie.findOneAndUpdate({ _id: req.params.id }, req.body, {new: true});
-    } catch(err) {
+    } catch (err) {
         return res.status(500).json({ message: err.message });
     }
-  })
+});
+
+// Update movie by id
+router.put('/movies/:id', async (req, res) => {
+    try {
+        const result = await Movie.findOneAndUpdate(
+            { _id: req.params.id },
+            req.body,
+            { new: true },
+        );
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+});
 
 module.exports = router;
