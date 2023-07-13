0ceb6164454988aaf1e7c47d9f4b480e1a9c1862
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-07-13 15:26:52 +0300

    feat: load and use router in the index.js file

--- 7.2-car-rest-api-mongodb-implementation/index.js
+++ 7.2-car-rest-api-mongodb-implementation/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require("./routes")
 
 const app = express();
 app.use(bodyParser.json());
+app.use('/', routes);
 
 //MongoDB connection
 const mongoose = require('mongoose');
