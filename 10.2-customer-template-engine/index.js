2af81b372d4ce7318c74eb4d816fc43e78933067
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-07-18 19:01:27 +0300

    feat: add route for GET /customerlist template-10.2

--- 10.2-customer-template-engine/index.js
+++ 10.2-customer-template-engine/index.js
@@ -4,6 +4,18 @@ const app = express();
 
 const port = 3000;
 
-app.listen(port, () => {
+app.set('view engine', 'pug');
+
+let customers = [
+    {id: '1588323375416', firstname: 'John', lastname: 'Johnson', email: 'john@johnson.com', phone: '8233243'},
+    {id: '1588323375417', firstname: 'Mary', lastname: 'Smith', email: 'mary@smith.com', phone: '6654113'},
+    {id: '1588323375418', firstname: 'Peter', lastname: 'North', email: 'peter@north.com', phone: '901176'},
+  ]
+
+app.get("/", (req, res) => {
+    res.render("customerlist", {customers: customers});
+})
+
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${port}.`);
 });
