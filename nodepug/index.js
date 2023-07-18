c2651d7db51490a367311215042d62f2cb87a256
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-07-18 18:30:41 +0300

    feat: add route for POST /addmovie template

--- nodepug/index.js
+++ nodepug/index.js
@@ -1,15 +1,19 @@
-const express = require('express');;
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000;
 
 app.set('view engine', 'pug');
 
 let movies = [
-  {id: '1588323375416', title: 'Star Wars: Episode IX - The Rise of Skywalker', year: 2019, director: 'J.J. Abrams'},
-  {id: '1588323390624', title: 'The Irishman', year: 2019, director: 'Martin Scorsese'},
-  {id: '1588323412643', title: 'Harry Potter and the Sorcerers Stone', year: 2001, director: 'Chris Columbus'}
+    {id: '1588323375416', title: 'Star Wars: Episode IX - The Rise of Skywalker', year: 2019, director: 'J.J. Abrams'},
+    {id: '1588323390624', title: 'The Irishman', year: 2019, director: 'Martin Scorsese'},
+    {id: '1588323412643', title: 'Harry Potter and the Sorcerers Stone', year: 2001, director: 'Chris Columbus'}
 ];
 
 app.get("/hello", (req, res) => {
@@ -21,7 +25,13 @@ app.get("/", (req, res) => {
 })
 
 app.get("/addmovie", (req, res) => {
-  res.render("addmovie");
+    res.render("addmovie");
+})
+
+app.post("/addmovie", (req, res) => {
+    const newMovie = {id: new Date().now, title: req.body.title, director: req.body.director, year: req.body.year};
+    movies = [...movies, newMovie];
+    res.redirect("/");
 })
 
 app.listen(process.env.PORT || 3000, () => {
